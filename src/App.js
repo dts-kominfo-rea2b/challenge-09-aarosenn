69ae529703c73c6ef2f9f8c70d95ebdce68b7c06
Anggoro Setyawan <anggoros90@gmail.com>
2022-06-26 18:50:51 +0700

    revise

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Header from './components/Header';
+import Contact from './components/Contact';
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
